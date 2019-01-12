98fd163acf55aa23ada1c8445daa185955e013dd
Dimitri Kopriwa <dka@dev-12.yeutech.vn>
2019-01-12 21:39:01 +0700

    fix(translation): remove many useless dependencies, remove the need of CoreAdmin, fix react-intl implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
-export { default, default as CoreAdmin } from './ra-core/CoreAdmin';
-export { default as TranslationProvider } from './ra-core/i18n/TranslationProvider';
+export { default, default as TranslationProvider } from './ra-core/i18n/TranslationProvider';
 export { TranslationContext } from './ra-core/i18n/TranslationContext';
